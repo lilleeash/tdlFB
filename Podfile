01fd3ea21aeeadc13e6df9523c505e99b0363ff9
Darya <lilleeash@icloud.com>
2022-05-11 10:36:06 +0300

    Edd SwiftGen in project

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+
+target 'tdlFB' do
+    pod 'SwiftGen', '~> 6.1'
+end
---
+++ Pods/Target Support Files/Pods-tdlFB/Pods-tdlFB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tdlFB : NSObject
+@end
+@implementation PodsDummy_Pods_tdlFB
+@end
