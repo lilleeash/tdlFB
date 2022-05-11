e2211d8b4bd309410a9f878b92e618f9ebfb881c
Darya <lilleeash@icloud.com>
2022-05-11 08:53:22 +0300

    Initial Commit

---
+++ tdlFB/tdlFBApp.swift
@@ -0,0 +1,17 @@
+//
+//  tdlFBApp.swift
+//  tdlFB
+//
+//  Created by Darya Zhitova on 11.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct tdlFBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
